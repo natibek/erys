150305c3786cece2e0b14f3cca18984b80c51da7
nati <nwtbekele@gmail.com>
2025-06-30 03:18:34 -0500

    update the code and markdown cells

--- code_cell.py
+++ code_cell.py
@@ -1,19 +1,50 @@
 from textual.app import ComposeResult
+from textual.reactive import reactive
 from textual.containers import HorizontalGroup, VerticalGroup
-from widgets import ExpandingTextArea
+from widgets import ExpandingTextArea, MIN_HEIGHT
+from textual.widgets import Button, Static, TextArea
 from typing import Any
 from utils import generate_id
+from textual.events import Key
+
+class CodeArea(TextArea):
+    offset_val: int = reactive(1)
+    def on_key(self, event: Key) -> None:
+        match event.key:
+            case "escape":
+                pass
+            case "enter":
+                cur_height = self.styles.height.cells
+                if cur_height + 1 > MIN_HEIGHT:
+                    self.styles.height = cur_height + 1
+                    exec_count = self.parent.query_one("#exec-count", Static)
+                    self.offset_val += 1
+                    exec_count.styles.margin = (self.offset_val, 0, 0, 1)
+            case "backspace":
+                if self.cursor_at_start_of_line:
+                    cur_height = self.styles.height.cells
+                    if cur_height - 1 > MIN_HEIGHT:
+                        self.styles.height = cur_height - 1
+                        exec_count = self.parent.query_one("#exec-count", Static)
+                        self.offset_val -= 1
+                        exec_count.styles.margin = (self.offset_val, 0, 0, 1)
+                    else:
+                        self.styles.height = MIN_HEIGHT
+                        exec_count = self.parent.query_one("#exec-count", Static)
+                        self.offset_val = 1
+                        exec_count.styles.margin = (1, 0, 0, 1)
 
 class OutputCell(ExpandingTextArea):
     def on_mount(self) -> None:
         self.disabled = True
 
-class CodeCell(ExpandingTextArea):
+class CodeCell(HorizontalGroup):
     def __init__(
             self, 
             source: str = "",
             output: str =  "",
             metadata: dict[str, Any] = {},
+            exec_count: int | None = None,
             cell_id: str | None = None,
         ) -> None:
         super().__init__()
@@ -21,10 +52,11 @@ class CodeCell(ExpandingTextArea):
         self.output = output
         self.metadata = metadata
         self.id = cell_id or generate_id()
+        self.exec_count = exec_count
 
     def compose(self) -> ComposeResult:
-        ExpandingTextArea.code_editor(self.source, language="python"),
-
-
-class CodeCell(VerticalGroup):
-    pass    
+        with HorizontalGroup():
+            with VerticalGroup(id="code-sidebar"):
+                yield Button(">", id="run-button")
+                yield Static(f"[{self.exec_count or ""}]", id="exec-count")
+            yield CodeArea.code_editor(self.source, language="python", id="code-editor")
--- markdown_cell.py
+++ markdown_cell.py
@@ -1,12 +1,30 @@
 from textual.app import ComposeResult
-from textual.widgets import Markdown, TextArea, ContentSwitcher, Collapsible
-from textual.events import Key
-from widgets import ExpandingTextArea, FocusMarkdown
+from textual.widgets import Markdown, TextArea, ContentSwitcher
+from textual.events import Key, MouseDown
+from widgets import ExpandingTextArea
+from textual.containers import HorizontalGroup
 from typing import Any
+from time import time
 from utils import generate_id
+
+PLACEHOLDER = "*Empty markdown cell, double-click or press enter to edit.*"
+class FocusMarkdown(Markdown):
+    can_focus = True
+
+    def on_mount(self) -> None:
+        if self.size == 0:
+            self.update(PLACEHOLDER)
+
+    def on_key(self, event: Key) -> None:
+        match event.key:
+            case "escape": 
+                pass
         
 
-class MarkdownCell(Collapsible):
+class MarkdownCell(HorizontalGroup):
+    double_click_interval = 0.3  # seconds
+    _last_click_time: float = 0.0
+
     def __init__(
             self, 
             source: str = "",
@@ -19,15 +37,26 @@ class MarkdownCell(Collapsible):
         self.cell_id = cell_id or generate_id()
 
     def compose(self) -> ComposeResult:
-        with ContentSwitcher(initial="raw-text", id="text-cell"):
+        with ContentSwitcher(initial="markdown", id="text-cell"):
             yield ExpandingTextArea(self.source, id="raw-text")
             yield FocusMarkdown(self.source, id="markdown")
 
     def on_key(self, event: Key) -> None:
         switcher = self.query_one("#text-cell", ContentSwitcher)
-        if event.key == "ctrl+e" and switcher.current == "raw-text":
+        if switcher.current == "raw-text" and event.key in {"ctrl+e", "escape"}:
             self.source= self.query_one("#raw-text", TextArea).text
             self.query_one("#markdown", Markdown).update(self.source)
             switcher.current = "markdown"
         elif event.key == "enter" and switcher.current == "markdown":
+            switcher.current = "raw-text"
+
+    def on_mouse_down(self, event: MouseDown) -> None:
+        now = time()
+        if now - self._last_click_time <= self.double_click_interval:
+            self.on_double_click(event)
+        self._last_click_time = now
+
+    def on_double_click(self, event: MouseDown) -> None: 
+        switcher = self.query_one("#text-cell", ContentSwitcher)
+        if switcher.current == "markdown":
             switcher.current = "raw-text"
