27764f18ad3db775aa4f67a1ee686b50e1a8cbf3
nati b <nwtbekele@gmail.com>
2025-07-16 00:04:32 -0500

    Documentation for save_as_screen.py

--- src/save_as_screen.py
+++ src/save_as_screen.py
@@ -9,7 +9,15 @@ from textual.validation import ValidationResult, Validator
 
 
 class NotebookName(Validator):
+    """Validator for notebook name."""
     def validate(self, value: str) -> ValidationResult:
+        """Checks if the provided name for a noteboook has correct ipynb extension.
+        
+        Args:
+            value: provided file name for notebook.
+
+        Returns: validation result based on whether extension is correct. 
+        """
         ext = Path(value).suffix
         if ext == ".ipynb":
             return self.success()
@@ -18,17 +26,28 @@ class NotebookName(Validator):
 
 
 class FilteredDirectoryTree(DirectoryTree):
+    """Filtered directory tree containing only directories and notebook files."""
     def filter_paths(self, paths: Iterable[Path]) -> Iterable[Path]:
         return [path for path in paths if path.suffix == ".ipynb" or path.is_dir()]
 
 
 class SaveAsScreen(Screen[str | None]):
     def compose(self) -> ComposeResult:
+        """Composed with:
+            - Screen
+                - Vertical (id=save-as)
+                    - Input (id=save-as-input)
+                    - Static (id=save-as-dir)
+                    - FilteredDirectoryTree (id=save-as-dir-tree)
+        """
         with Vertical(id="save-as"):
+            # input for file name
             self.input = Input(
                 placeholder="File Name", id="save-as-input", validators=[NotebookName()]
             )
+            # displays the directory where the file will be saved at
             self.cur_dir = Static(f"Saving at: {Path.cwd()}", id="save-as-dir")
+            # to help select the directory where the file can be saved at
             self.dir_tree = FilteredDirectoryTree(Path.cwd(), id="save-as-dir-tree")
 
             yield self.cur_dir
@@ -36,40 +55,76 @@ class SaveAsScreen(Screen[str | None]):
             yield self.dir_tree
 
     def on_screen_resume(self, event: ScreenResume) -> None:
+        """Screen resume event handler that resets the path for directory tree and static to pwd.
+        
+        Args:
+            event: screen resume event.
+        """
         self.cur_dir.update(f"Saving at: {Path.cwd()}")
         self.dir_tree.path = Path.cwd()
 
     def on_directory_tree_file_selected(
         self, event: DirectoryTree.FileSelected
     ) -> None:
+        """Directory tree file selected event handler that sets the file name for saving file to
+        the name of the selected file and updates the directory where the file will be saved.
+        
+        Args:
+            event: directory tree file selected event.
+        """
         file_dir = event.path.parent
         self.cur_dir.update(f"Saving at: {file_dir}")
 
         file_name = event.path.name
+        # clear and update the input widget to show selected file's file name
         self.input.clear()
         self.input.insert(file_name, 0)
+        # don't propagate this event to other handlers
         event.stop()
 
     def on_directory_tree_directory_selected(
         self, event: DirectoryTree.DirectorySelected
     ) -> None:
+        """Directory tree directory selected event handler that sets the directory where
+        file will be saved to the selected directory.
+        
+        Args:
+            event: directory tree directory selected event.
+        """
         self.dir_tree.path = event.path
         self.cur_dir.update(f"Saving at: {event.path}")
+        # don't propagate this event to other handlers
         event.stop()
 
     def on_key(self, event: Key) -> None:
+        """Key press event handler that 
+            - prevents event propagation for the escape, n, ctrl+k, ctrl+l, d keys
+            - used backspace for navigating up a directory
+        
+        Args:
+            event: key event.
+        """
         match event.key:
             case "escape":
                 self.dismiss(None)
+                # don't propagate this event to other handlers
                 event.stop()
             case "backspace" if self.app.focused == self.dir_tree:
                 parent = Path(self.dir_tree.path).resolve().parent
                 self.dir_tree.path = parent
                 self.cur_dir.update(f"Saving at: {parent}")
             case "n" | "ctrl+k" | "ctrl+l" | "d":
+                # don't propagate this event to other handlers
                 event.stop()
 
     def on_input_submitted(self, event: Input.Submitted) -> None:
+        """Input submitted event handler that dismisses the screen with the file path if
+        input value is valid; otherwise notifies that the input value is not a valid file name.
+
+
+        Args:
+            event: input submitted event.
+        """
         if event.validation_result.is_valid:
             file_path = self.dir_tree.path.joinpath(event.value)
             self.dismiss(file_path)
