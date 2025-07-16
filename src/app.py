7cb2fd9db4faedfa209e020b5cc5b1f2d9aba8e9
nati b <nwtbekele@gmail.com>
2025-07-16 00:25:49 -0500

    Documentation for app.py

--- src/app.py
+++ src/app.py
@@ -25,6 +25,13 @@ class QuitScreen(Screen):
     """Screen with a dialog to quit."""
 
     def compose(self) -> ComposeResult:
+        """Composed with:
+            - Screen
+                - Grid (id=dialog)
+                    - Label (id=question)
+                    - Button (id=quit)
+                    - Button (id=cancel)
+        """
         yield Grid(
             Label("Are you sure you want to quit?", id="question"),
             Button("Quit", variant="error", id="quit"),
@@ -33,12 +40,22 @@ class QuitScreen(Screen):
         )
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Button pressed event handler that quits app or pops screen.
+        
+        Args:
+            event: button pressed event.
+        """
         if event.button.id == "quit":
             self.app.exit()
         else:
             self.app.pop_screen()
 
     def on_key(self, event: Key) -> None:
+        """Key event handler that pops screen when escapa is pressed.
+        
+        Args:
+            event: key event.
+        """
         match event.key:
             case "escape":
                 self.app.pop_screen()
@@ -46,23 +63,30 @@ class QuitScreen(Screen):
 
 
 class DirectoryNav(DirectoryTree):
+    """Custom directory tree with key navigation."""
     BINDINGS = [
-        ("backspace", "back_dir", "Go back up directory"),
+        ("backspace", "back_dir", "Go up a directory"),
     ]
-    selected_dir: str | None = None
+    selected_dir: str | None = None # keep track of the selected directory
 
     def action_back_dir(self) -> None:
+        """Moves up a directory."""
         parent = Path(self.path).resolve().parent
         self.path = parent
 
     def on_directory_tree_directory_selected(
         self, event: DirectoryTree.DirectorySelected
     ) -> None:
+        """Directory selected event handler that sets the current directory to selected.
+        
+        Args:
+            event: directory selected event.
+        """
         self.path = Path(event.path).resolve()
 
 
 class TerminalNotebook(App):
-    """A Textual app to manage stopwatches."""
+    """A Textual app for editing and running Python notebooks."""
 
     CSS_PATH = "styles.tcss"
     SCREENS = {"quit_screen": QuitScreen, "save_as_screen": SaveAsScreen}
@@ -74,21 +98,30 @@ class TerminalNotebook(App):
         ("ctrl+q", "push_screen('quit_screen')", "Quit"),
     ]
 
-    tab_id = reactive("")
-
     def __init__(self, paths: list[str]) -> None:
         super().__init__()
         self.theme = "textual-dark"
+        # check if the provided file paths are python notebooks
         self.paths = [
             os.path.relpath(path, Path.cwd())
             for path in paths
             if Path(path).is_file() and Path(path).suffix == ".ipynb"
         ]
         self.cur_tab = len(paths)
-        self.tab_to_nb_id_map: dict[str, int] = {}
+        self.tab_to_nb_id_map: dict[str, int] = {} # maps from tab id to notebook id
 
     def compose(self) -> ComposeResult:
-        """Create child widgets for the app."""
+        """Composed with:
+            - App
+                - Header
+                - Horizontal
+                    - DirectoryNav (id=file-tree) 
+                    - Vertical
+                        - Tabs
+                        - ContentSwitcher (id=tab-content)
+                            - Notebook
+                - Footer
+        """
 
         yield Header(show_clock=True, time_format="%I:%M:%S %p")
 
@@ -110,7 +143,9 @@ class TerminalNotebook(App):
         yield Footer()
 
     def on_mount(self) -> None:
-        """Focus the tabs when the app starts."""
+        """Mount event handler that creates a new notebook if none are opened when app starts,
+        then focuses on the tabs.
+        """
         if len(self.paths) == 0:
             self.action_new_notebook()
 
@@ -118,7 +153,11 @@ class TerminalNotebook(App):
         self.dir_tree.display = False
 
     def on_tabs_tab_activated(self, event: Tabs.TabActivated) -> None:
-        """Handle TabActivated message sent by Tabs."""
+        """Tab activated event handler that switches content to show notebook the tab belongs to.
+
+        Args:
+            event: tab activated event. 
+        """
         if event.tab is None:
             pass
         else:
@@ -128,6 +167,12 @@ class TerminalNotebook(App):
     def on_directory_tree_file_selected(
         self, event: DirectoryTree.FileSelected
     ) -> None:
+        """File selected event handler that opens notebook if selected file path is has valid
+        extension.
+
+        Args:
+            event: file selected event. 
+        """
         if not os.path.exists(event.path):
             self.notify(f"{event.path} does not exist.", severity="error", timeout=8)
             return
@@ -141,6 +186,13 @@ class TerminalNotebook(App):
         self.open_notebook(event.path)
 
     def on_key(self, event: Key) -> None:
+        """Key event handler that
+            - moves focus to the Tabs when escape is pressed
+            - moves focus to latest Notebook when enter is pressed (and tabs was focused on)
+
+        Args:
+            event: key event. 
+        """
         match event.key:
             case "escape":
                 self.set_focus(self.tabs)
@@ -148,6 +200,7 @@ class TerminalNotebook(App):
                 if not self.switcher.current:
                     return
 
+                # if currently focused on the tabs, change focus to the latest notebook
                 if isinstance(self.app.focused, Tabs):
                     notebook = self.switcher.query_one(
                         f"#{self.switcher.current}", Notebook
@@ -155,6 +208,7 @@ class TerminalNotebook(App):
                     self.call_next(notebook.focus_notebook)
 
     def action_toggle_directory_tree(self) -> None:
+        """Toggle whether the directory tree is displayed."""
         self.dir_tree.display = not self.dir_tree.display
         if self.dir_tree.display:
             self.set_focus(self.dir_tree)
@@ -162,13 +216,19 @@ class TerminalNotebook(App):
             self.set_focus(self.tabs)
 
     def action_new_notebook(self) -> None:
+        """Create a new notebook with the path set to 'new_empty_terminal_notebook'."""
+        # for a new notebook the notebook id is the same as the tab id
         tab_id = f"tab{self.cur_tab}"
+        # add a new tab
         self.tabs.add_tab(Tab(tab_id, id=tab_id))
         self.tab_to_nb_id_map[tab_id] = tab_id
 
         new_notebook = Notebook("new_empty_terminal_notebook", tab_id, self)
         self.switcher.mount(new_notebook)
 
+        # set the new tab to be the active one
+        # no need to change what is the current notebook since that will be set by the
+        # tab activated event handler
         self.tabs.active = tab_id
         self.cur_tab += 1
 
@@ -176,12 +236,14 @@ class TerminalNotebook(App):
         """Remove active tab."""
         active_tab = self.tabs.active_tab
         # TODO: ask for save
+        # if a tab is active then remove it and the notebook
         if active_tab is not None:
             self.tabs.remove_tab(active_tab.id)
             notebook_id = self.tab_to_nb_id_map[active_tab.label]
             self.switcher.remove_children(f"#{notebook_id}")
             del self.tab_to_nb_id_map[active_tab.label]
 
+        # set the switcher's currently displayed widget to none to avoid errors 
         if len(self.tab_to_nb_id_map) == 0:
             self.switcher.current = None
 
@@ -190,6 +252,8 @@ class TerminalNotebook(App):
         self.tabs.clear()
         for child in self.switcher.children:
             child.remove()
+
+        # clear the map and the switcher's currently displayed widget to avoid errors
         self.tab_to_nb_id_map = {}
         self.switcher.current = None
 
